85a164f8ebad56feb957e425d3cbdd532dd301c8
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-02-08 19:22:35 +0000

    Deploying to gh-pages from @ valiana35/goit-js-hw-11@8db06f749d9bf752c81b35b836e3baa1a8f0ee9f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,10 +1,16 @@
-import{i as a,S as c}from"./assets/vendor-5b791d57.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();const l=document.querySelector(".form");document.querySelector(".form-input");document.querySelector(".btn");const u=document.querySelector(".gallery");l.addEventListener("submit",f);const m=new URLSearchParams({key:42256594-6,q:`"${image.tags}"`,image_type:"photo",orientation:"horizontal",safesearch:!0});function f(){fetch(`https://pixabay.com/api/images?${m}`).then(t=>(t.ok||a.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),t.json())).then(t=>{const o=t.map(s=>`<li class="gallery-item">
-            <a class="gallery-link" href="${s.largeImageURL}">
-                <img 
-                    class="gallery-image"
-                    src="${s.webformatURL}" 
-                    alt="${s.tags}"
-                />
-            </a>
-            </li>`).join("");u.insertAdjacentHTML("beforeend",o)})}new c(".gallery a",{});
+import{i as m,S as f}from"./assets/vendor-5b791d57.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function t(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function s(e){if(e.ep)return;e.ep=!0;const o=t(e);fetch(e.href,o)}})();const l=document.querySelector(".form"),p=document.querySelector(".form-input");document.querySelector(".btn");const a=document.querySelector(".gallery"),c=document.querySelector(".span"),d=new URLSearchParams({key:"42127236-8bfdbbfbeed8a2dadaca720e8",q:`${encodeURIComponent(p.value)}`,image_type:"photo",orientation:"horizontal",safesearch:!0});l.addEventListener("submit",n=>{n.preventDefault(),c.classList.add("loader"),a.innerHTML="",h(d).then(r=>u(r)).catch(r=>console.log(r)).finally(()=>c.remove()),l.reset()});function h(n){const r=`https://pixabay.com/api/?${n}`;return fetch(r).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()}).then(t=>{t.hits.length===0?m.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}):u(t)})}function u(n){const r=n.hits.map(s=>`<li class="gallery-item">
+        <a class="gallery-link" href="${s.largeImageURL}">
+            <img 
+                class="gallery-image"
+                src="${s.webformatURL}" 
+                alt="${s.tags}"
+            />
+        </a>
+        <div class="description">
+        <p><b>Likes: </b>${s.likes}</p>
+        <p><b>Views: </b>${s.views}</p>
+        <p><b>Comments: </b>${s.comments}</p>
+        <p><b>Downloads: </b>${s.downloads}</p>
+        </div>
+        </li>`).join("");a.innerHTML=r;let t=new f(".gallery a",g);t.on("show.simplelightbox"),t.refresh()}const g={captionDelay:250};
 //# sourceMappingURL=commonHelpers.js.map
